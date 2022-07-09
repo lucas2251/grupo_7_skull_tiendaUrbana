a03d66e248c8ba44a199bc1bd29b262d8c9920e9
SebastianCarballo <sebastiancarballo.sc.sc@gmail.com>
2022-07-09 19:17:52 -0300

    arreglo las direcciones en app.js

--- site/app.js
+++ site/app.js
@@ -5,10 +5,10 @@ const port = 3000;
 
 app.use(express.static(path.resolve(__dirname,'public')));
 
-app.getMaxListeners('/',(req,res) => res.sendFile(path.resoleve(__dirname,'views','home.html')));
-app.getMaxListeners('/detalle',(req,res) => res.sendFile(path.resoleve(__dirname,'views','detalle.html')));
-app.getMaxListeners('/carrito',(req,res) => res.sendFile(path.resoleve(__dirname,'views','carrito.html')));
-app.getMaxListeners('/login',(req,res) => res.sendFile(path.resoleve(__dirname,'views','login.html')));
-app.getMaxListeners('/register',(req,res) => res.sendFile(path.resoleve(__dirname,'views','register.html')));
+app.getMaxListeners('/',(req,res) => res.sendFile(path.resolve(__dirname,'views','home.html')));
+app.getMaxListeners('/detalle',(req,res) => res.sendFile(path.resolve(__dirname,'views','detalle.html')));
+app.getMaxListeners('/carrito',(req,res) => res.sendFile(path.resolve(__dirname,'views','carrito.html')));
+app.getMaxListeners('/login',(req,res) => res.sendFile(path.resolve(__dirname,'views','login.html')));
+app.getMaxListeners('/register',(req,res) => res.sendFile(path.resolve(__dirname,'views','register.html')));
 
 app.listen(port,() => console.log(`Servidor corriendo en http://localhost:${port}`));

ed05d364b12e2787b9fcf633a880379a8ad410f2
SebastianCarballo <sebastiancarballo.sc.sc@gmail.com>
2022-07-09 10:43:53 -0300

    termino de crear la estructura dentro del archivo app.js

--- site/app.js
+++ site/app.js
@@ -4,3 +4,11 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.resolve(__dirname,'public')));
+
+app.getMaxListeners('/',(req,res) => res.sendFile(path.resoleve(__dirname,'views','home.html')));
+app.getMaxListeners('/detalle',(req,res) => res.sendFile(path.resoleve(__dirname,'views','detalle.html')));
+app.getMaxListeners('/carrito',(req,res) => res.sendFile(path.resoleve(__dirname,'views','carrito.html')));
+app.getMaxListeners('/login',(req,res) => res.sendFile(path.resoleve(__dirname,'views','login.html')));
+app.getMaxListeners('/register',(req,res) => res.sendFile(path.resoleve(__dirname,'views','register.html')));
+
+app.listen(port,() => console.log(`Servidor corriendo en http://localhost:${port}`));

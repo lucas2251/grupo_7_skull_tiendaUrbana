093dfc75b99f07bc4ba0cd6dbec2e7bab75a665b
Sebastian Carballo <sebastiancarballo.sc.sc@gmail.com>
2022-07-11 19:46:40 -0300

    Empiezo armar la estructura para ver los cambios en tiempo real con nodemon

--- site/app.js
+++ site/app.js
@@ -1,16 +1,31 @@
+const livereload = require("livereload");
+const liveReloadServer = livereload.createServer();
+
+
+const connectLivereload = require("connect-livereload");
 const express = require('express');
 const path = require('path');
+const { connect } = require("http2");
 const app = express();
 const port = 3000;
 
 app.use(express.static(path.resolve(__dirname,'public')));
 
-app.get('/',(req,res) => res.sendFile(path.resolve(__dirname,'views','home.html')))
+liveReloadServer.watch(path.join(__dirname,'public'));
 
-app.getMaxListeners('/',(req,res) => res.sendFile(path.resolve(__dirname,'views','home.html')));
-app.getMaxListeners('/detalle',(req,res) => res.sendFile(path.resolve(__dirname,'views','detalle.html')));
-app.getMaxListeners('/carrito',(req,res) => res.sendFile(path.resolve(__dirname,'views','carrito.html')));
-app.getMaxListeners('/login',(req,res) => res.sendFile(path.resolve(__dirname,'views','login.html')));
-app.getMaxListeners('/register',(req,res) => res.sendFile(path.resolve(__dirname,'views','register.html')));
+app.use(connectLivereload());
+
+
+app.get('/',(req,res) => res.sendFile(path.resolve(__dirname,'views','home.html')));
+app.get('/detalle',(req,res) => res.sendFile(path.resolve(__dirname,'views','detalle.html')));
+app.get('/carrito',(req,res) => res.sendFile(path.resolve(__dirname,'views','carrito.html')));
+app.get('/login',(req,res) => res.sendFile(path.resolve(__dirname,'views','login.html')));
+app.get('/register',(req,res) => res.sendFile(path.resolve(__dirname,'views','register.html')));
+
+liveReloadServer.server.once("connection",() => {
+    setTimeout(() => {
+        liveReloadServer.refresh("/")
+    }, 100);
+});
 
 app.listen(port,() => console.log(`Servidor corriendo en http://localhost:${port}`));

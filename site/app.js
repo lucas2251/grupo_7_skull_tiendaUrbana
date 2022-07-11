1e3390779d48c2cadda8708e3aad608141d5f0a5
Sebastian Carballo <sebastiancarballo.sc.sc@gmail.com>
2022-07-11 20:34:17 -0300

    Arreglo los enlaces get de app.js

--- site/app.js
+++ site/app.js
@@ -5,10 +5,10 @@ const port = 3000;
 
 app.use(express.static(path.resolve(__dirname,'public')));
 
-app.getMaxListeners('/',(req,res) => res.sendFile(path.resolve(__dirname,'views','home.html')));
-app.getMaxListeners('/detalle',(req,res) => res.sendFile(path.resolve(__dirname,'views','detalle.html')));
-app.getMaxListeners('/carrito',(req,res) => res.sendFile(path.resolve(__dirname,'views','carrito.html')));
-app.getMaxListeners('/login',(req,res) => res.sendFile(path.resolve(__dirname,'views','login.html')));
-app.getMaxListeners('/register',(req,res) => res.sendFile(path.resolve(__dirname,'views','register.html')));
+app.get('/',(req,res) => res.sendFile(path.resolve(__dirname,'views','home.html')));
+app.get('/detalle',(req,res) => res.sendFile(path.resolve(__dirname,'views','detalle.html')));
+app.get('/carrito',(req,res) => res.sendFile(path.resolve(__dirname,'views','carrito.html')));
+app.get('/login',(req,res) => res.sendFile(path.resolve(__dirname,'views','login.html')));
+app.get('/register',(req,res) => res.sendFile(path.resolve(__dirname,'views','register.html')));
 
 app.listen(port,() => console.log(`Servidor corriendo en http://localhost:${port}`));

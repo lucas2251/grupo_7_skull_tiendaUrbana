f7db5e1e069761d031e369dc63d414ee9a0233bb
Sebastian Carballo <sebastiancarballo.sc.sc@gmail.com>
2022-07-14 12:28:39 -0300

    Creo el archivo contacto. html y css, y le doy al rura en app.js

--- site/app.js
+++ site/app.js
@@ -21,6 +21,7 @@ app.get('/detalle',(req,res) => res.sendFile(path.resolve(__dirname,'views','det
 app.get('/carrito',(req,res) => res.sendFile(path.resolve(__dirname,'views','carrito.html')));
 app.get('/login',(req,res) => res.sendFile(path.resolve(__dirname,'views','login.html')));
 app.get('/register',(req,res) => res.sendFile(path.resolve(__dirname,'views','register.html')));
+app.get('/contacto',(req,res) => res.sendFile(path.resolve(__dirname,'views','contacto.html')));
 
 liveReloadServer.server.once("connection",() => {
     setTimeout(() => {

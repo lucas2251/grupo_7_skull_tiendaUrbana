4a8fea885c2c927c647c637f127d75c30306ac9c
lucas2251 <lucascordoba2@hotmail.com>
2022-08-02 00:42:10 -0300

    alinie el precio del carrito

--- site/app.js
+++ site/app.js
@@ -6,7 +6,7 @@ const connectLivereload = require("connect-livereload");
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 3030;
 
 app.use(express.static(path.resolve(__dirname,'public')));
 

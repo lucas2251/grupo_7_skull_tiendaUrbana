7a831caeb060bae133af5cacf28bb60f55794fb0
lucas2251 <lucascordoba2@hotmail.com>
2022-08-09 00:07:31 -0300

    termino primera parte del sprint 3

--- site/app.js
+++ site/app.js
@@ -15,13 +15,25 @@ liveReloadServer.watch(path.join(__dirname,'public'));
 app.use(connectLivereload());
 
 
-app.get('/',(req,res) => res.sendFile(path.resolve(__dirname,'views','home.html')));
-app.get('/detalle',(req,res) => res.sendFile(path.resolve(__dirname,'views','detalle.html')));
-app.get('/carrito',(req,res) => res.sendFile(path.resolve(__dirname,'views','carrito.html')));
-app.get('/login',(req,res) => res.sendFile(path.resolve(__dirname,'views','login.html')));
-app.get('/register',(req,res) => res.sendFile(path.resolve(__dirname,'views','register.html')));
-app.get('/contacto',(req,res) => res.sendFile(path.resolve(__dirname,'views','contacto.html')));
-app.get("*",(req,res) => res.sendFile(path.resolve(__dirname, 'views','404.html')))
+/* Requerir las rutas */
+let indexRouter = require('./routes/index')
+//let administradorRouter = require('./routes/administrador')
+let productosRouter = require('./routes/productos')
+let usuariosRouter = require('./routes/usuarios')
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+/* Middlewares */
+app.use(express.json()); //estes
+app.use(express.static(path.resolve(__dirname,'public')))
+
+/* Rutas */
+app.use('/', indexRouter);
+app.use('/usuarios', usuariosRouter);
+app.use('/productos', productosRouter);
+//app.use('/administrador',administradorRouter);
 
 liveReloadServer.server.once("connection",() => {
     setTimeout(() => {

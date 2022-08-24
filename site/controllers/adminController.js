a38f74ac3bc3124cecbfd6c98bfbe9b22ef0ced7
lucas2251 <lucascordoba2@hotmail.com>
2022-08-24 00:38:13 -0300

    vuelvo dinamica la vista de detalle de producto y empiezo a trabajar con put

--- site/controllers/adminController.js
+++ site/controllers/adminController.js
@@ -14,10 +14,10 @@ module.exports = {
     },
     editar:(req,res) => {
 
-        /* id = +req.params.id
+        id = +req.params.id
         let producto = productos.find((elemento) => {
             return elemento.id == id
-        }) */
+        }) 
         /* return res.send(producto) Comprobar que esta llegando bien el elemento*/
        return res.render('admin/editar')
     }
--- site/routes/administrador.js
+++ site/routes/administrador.js
@@ -7,7 +7,7 @@ const router = express.Router()
 let {crear,editar,listar} = require('../controllers/adminController')
 
 router.get('/listar', listar)
-router.get('/editar', editar)
+router.put('/editar/:id', editar)
 router.get('/crear', crear)
 
 module.exports = router

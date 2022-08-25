9ff3e9efa2cb3b91e7e770aa9c58ad26ac3d5b56
lucas2251 <lucascordoba2@hotmail.com>
2022-08-25 02:42:07 -0300

    doy primeros funcionamientos al edit

--- site/controllers/adminController.js
+++ site/controllers/adminController.js
@@ -1,5 +1,8 @@
 let productos = require("../data/productos.json");
-
+const fs = require('fs')
+const path = require('path')
+const guardar = (dato) => fs.writeFileSync(path.join(__dirname, '../data/productos.json')
+    , JSON.stringify(dato, null, 4), 'utf-8')
 
 module.exports = {
 
@@ -20,6 +23,24 @@ module.exports = {
         }) 
         /* return res.send(producto) Comprobar que esta llegando bien el elemento*/
        return res.render('admin/editar',{producto})
+    },
+    actualizar:(req,res)=>{
+        const idParams = +req.params.id
+        const { Marca, Titulo, Categoria, Precio, Descuento, Stock, Descripcion } = req.body
+
+        productos.forEach(producto => {
+            if (producto.id === idParams) {
+                producto.marca = Marca
+                producto.titulo = Titulo
+                producto.categoria = Categoria
+                producto.precio = +Precio
+                producto.descuento = +Descuento
+                producto.stock = +Stock
+                producto.descripcion = Descripcion
+            }
+        })
+        guardar(productos)
+            return res.redirect('/administrador/listar')
     }
 
     }

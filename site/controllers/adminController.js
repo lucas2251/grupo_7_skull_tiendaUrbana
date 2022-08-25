61da3f89cdb3200337d973c522acd04b43a4c49f
yesifiore91 <jesicafiore1@gmail.com>
2022-08-25 12:20:01 -0300

    agregue la variable guardar a adminontroller

--- site/controllers/adminController.js
+++ site/controllers/adminController.js
@@ -1,10 +1,14 @@
-let productos = require("../data/productos.json");
-let lijas = require("../data/partes/lijas.json");
-let otros = require("../data/partes/otros.json");
-let ruedas = require("../data/partes/ruedas.json");
-let tablas = require("../data/partes/tablas.json");
-let trucks = require("../data/partes/trucks.json")
+const productos = require("../data/productos.json");
+const lijas = require("../data/partes/lijas.json");
+const otros = require("../data/partes/otros.json");
+const ruedas = require("../data/partes/ruedas.json");
+const tablas = require("../data/partes/tablas.json");
+const trucks = require("../data/partes/trucks.json");
 
+const guardar = (dato) => fs.writeFileSync(path.join(__dirname, '../data/productos.json')
+    , JSON.stringify(dato, null, 4), 'utf-8')
+
+    
 module.exports = {
 
 

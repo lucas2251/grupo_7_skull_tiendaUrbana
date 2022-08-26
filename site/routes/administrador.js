48a8894c4b71b32792a0ad62151b86b38562d29f
lucas2251 <lucascordoba2@hotmail.com>
2022-08-26 01:28:08 -0300

    agrego ruta post

--- site/routes/administrador.js
+++ site/routes/administrador.js
@@ -4,10 +4,14 @@ const router = express.Router()
 
 
 
-let {crear,editar,listar,actualizar} = require('../controllers/adminController')
+
+let {crear,editar,listar,actualizar,tienda} = require('../controllers/adminController')
+
 
 router.get('/listar', listar)
+
 router.get('/crear', crear)
+router.post("/crear", tienda)
 
 router.get('/editar/:id', editar)
 router.put('/editar/:id', actualizar)
